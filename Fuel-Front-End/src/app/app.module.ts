6f25430bead3386e699be5fa8a05934e36686233
DEAN <wolnguyen98@gmail.com>
2020-02-08 19:07:11 -0600

    login

--- Fuel-Front-End/src/app/app.module.ts
+++ Fuel-Front-End/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Hero_imageComponent } from './hero_image/hero_image.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavbarComponent,
-      Hero_imageComponent
+      Hero_imageComponent,
+      LoginComponent
    ],
    imports: [
       BrowserModule,
