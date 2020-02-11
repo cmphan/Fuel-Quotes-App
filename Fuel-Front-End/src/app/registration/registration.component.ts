d7ced619aac3955c1e86697947f7d5793e6d24cf
DEAN <wolnguyen98@gmail.com>
2020-02-11 15:21:53 -0600

    update-logo-and-register-page

--- Fuel-Front-End/src/app/registration/registration.component.ts
+++ Fuel-Front-End/src/app/registration/registration.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
 
